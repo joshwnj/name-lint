39b5a57a516bb7cd3f1d4dc0c4687b307fb31b72
Josh Johnston <josh@x-team.com>
2014-09-10 13:56:03 +1000

    added repo to package.json + added .editorconfig file

--- examples/snake-case.js
+++ examples/snake-case.js
@@ -6,24 +6,24 @@ var opts = {};
 opts.dirFormat = lowerSnakeCase;
 
 opts.fileFormats = {
-    // and js / html files as well
-    '.js': lowerSnakeCase,
-    '.html': lowerSnakeCase,
+  // and js / html files as well
+  '.js': lowerSnakeCase,
+  '.html': lowerSnakeCase,
 
-    // scss files sometimes start with an underscore to indicate a mixin
-    '.scss': /^_?[a-z0-9\-]+$/
+  // scss files sometimes start with an underscore to indicate a mixin
+  '.scss': /^_?[a-z0-9\-]+$/
 };
 
 // exclude directories / files that match anything in this list
 // (uses https://www.npmjs.org/package/minimatch)
 opts.exclude = [
-    '**/node_modules',
-    '**/build'
+  '**/node_modules',
+  '**/build'
 ];
 
 var dir = process.argv[2] || '.';
 lint(dir, opts, function (err, matches) {
-    if (err) { throw err; }
+  if (err) { throw err; }
 
-    console.log(matches.join('\n'));
+  console.log(matches.join('\n'));
 });
